3d8d74ae4e7fed4ebd19beb0ee00d88d65ea74ac
Edu <eduardogalli89@gmail.com>
2024-06-15 11:14:07 -0300

    Correcion en items que no se monstraban

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,43 +1,58 @@
-import { useContext, useEffect, useState } from "react";
-import { CartContext } from "./context/CartContext";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+//import arrayProductos from "./json/productos.json";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 
 const Checkout = () => {
-    const {cart, getCountProducts, getSumProducts} = useContext(CartContext);
+    const [cart, setCart] = useState([]);
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
     const [telephone, setTelephone] = useState("");
     const [orderId, setOrderId] = useState("");
+    
+/*useEffect(() => {
+    const promesa = new Promise(resolve => {
+        setTimeout(() => {
+            resolve(arrayProductos.filter(item => item.precio <500000));
+        }, 2000)
+    });
+    promesa.then(respuesta => {
+        setCart(respuesta);
+    })
+},[])*/
 
-    const generarOrden = () => {
-        const buyer = {nombre:nombre, email:email, telephone:telephone};
-        const items = cart.map(item => ({id:item.id, title:item.nombre, price:item.precio}));
-        const date = new Date();
-        const currentDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-        const order = {buyer:buyer, items:items, date:currentDate, total:getSumProducts()};
-        
+useEffect(() => {
     const db = getFirestore();
-        const ordersCollection = collection(db, "orders");
-        addDoc(ordersCollection, order).then(data => {
-            setOrderId(data.id);
-        });
-    }
+    const itemsCollection = collection(db, "items");
+    getDocs(itemsCollection).then(snapShot => {
+        if (snapShot.size > 0) {
+            setCart(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
+        }
+    })
+},[]);
 
-    if (getCountProducts() == 0) {
-        return (
-            <div className="container my-5">
-                <div className="row">
-                    <div className="col text-center">
-                        <h3>No se encontraron Productos en el Carrito!</h3>
-                        <Link to={"/"} className="btn text-white bg-dark rounded-0 my-5">Volver a la Página Principal</Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+const calcularTotal = () => {
+    return cart.reduce((acumulador,item) => acumulador += item.precio, 0);
+}
 
-    return (
+const generarOrden = () => {
+    const buyer = {nombre:nombre, email:email, telephone:telephone};
+    const items = cart.map(item => ({id:item.id, title:item.nombre, price:item.precio}))
+    const order = {buyer:buyer, items:items, total:calcularTotal()};
+    //console.log(order);
+    /*const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order).then(data => {
+        setOrderId(data.id);
+    });*/
+    const db = getFirestore();
+    const itemsCollection = collection(db, "items");
+    arrayProductos.forEach(item => {
+        addDoc(itemsCollection, item);
+    })
+    console.log("Proceso de carga de productos completa");
+}
+
+return (
         <div className="container my-5">
             <div className="row">
                 <div className="col">
@@ -65,23 +80,25 @@ const Checkout = () => {
                                 <tr key={item.id}>
                                     <td><img src={item.imagen} alt={item.nombre} width={80} /></td>
                                     <td>{item.nombre}</td>
-                                    <td>x{item.quantity}</td>
                                     <td className="text-end">${item.precio}</td>
                                 </tr>
                             ))}
                             <tr>
-                                <td colSpan={3}><b>Total</b></td>
-                                <td className="text-end"><b>${getSumProducts()}</b></td>
+                                <td colSpan={2}>Total</td>
+                                <td className="text-end">${calcularTotal()}</td>
                             </tr>
                         </tbody>
+
                     </table>
                 </div>
-            </div>
+            </div>    
             <div className="row my-5">
                 <div className="col text-center">
-                    {orderId ? <div className="alert alert-light" role="alert">Felicitaciones! Tu ID de Compra es: <b>{orderId}</b></div> : ""}
+                    {orderId ? <div className="alert alert-light" role="alert">Felicitaciones tu Id de compra es: <b>{orderId}</b></div> :""}
                 </div>
-            </div>
+            
+            </div>        
+            
         </div>
     )
 }
